5e488a2a033afda5ed1bf9b4957ba4c84628c6b7
Paul <paul@the7thdream.com>
2026-07-14 19:39:08 +0800

    llmrouter & scriptling

--- Formula/llmrouter.rb
+++ Formula/llmrouter.rb
@@ -2,22 +2,22 @@ class Llmrouter < Formula
 	desc "A unified gateway that aggregates multiple LLM providers behind a single endpoint"
 	homepage "https://github.com/paularlott/llmrouter"
 	license "MIT"
-	version "0.6.2"
+	version "0.6.3"
 	if OS.mac?
 		if Hardware::CPU.arm?
 			url "https://github.com/paularlott/llmrouter/releases/download/v#{version}/llmrouter-darwin-arm64.zip"
-			sha256 "48c6445d85069c2f1ff30d4454d38fc4da45022ae2d7142c1b68631a7b52d7a5"
+			sha256 "b45daa26d66b15d6eb08d80370502ab8d2b21a3b251e3700c08b5e17a7dce7c2"
 		else
 			url "https://github.com/paularlott/llmrouter/releases/download/v#{version}/llmrouter-darwin-amd64.zip"
-			sha256 "c12d0db6bf583f0c41b6e7d7193e089da864877fd3c2d3f8de8f535cc532aac9"
+			sha256 "4216df23242d56f3f55a06cb8ef1c230ddbb83c31a2be7904744387a67783670"
 		end
 	elsif OS.linux?
 		if Hardware::CPU.arm?
 			url "https://github.com/paularlott/llmrouter/releases/download/v#{version}/llmrouter-linux-arm64.zip"
-			sha256 "9f0d406211ea8fdb82a58b0bae44d94fc229a3ffba7277758cab611393411368"
+			sha256 "31e9851f7f7a8d72b9e7f061a58a9019b9667dc01b0b2313e27e9d65c543296f"
 		else
 			url "https://github.com/paularlott/llmrouter/releases/download/v#{version}/llmrouter-linux-amd64.zip"
-			sha256 "826353421ab4b18ea077d83348942574c660e59f67b819804f71281d83f6e80a"
+			sha256 "b14d38521e342c24e0581973da2a301ad23d8a23574b3a3d2df41d59e31de099"
 		end
 	end
 
--- Formula/scriptling.rb
+++ Formula/scriptling.rb
@@ -2,22 +2,22 @@ class Scriptling < Formula
 	desc "A powerful scripting language with Python-like syntax and Go performance"
 	homepage "https://github.com/paularlott/scriptling"
 	license "MIT"
-	version "0.17.1"
+	version "0.17.2"
 	if OS.mac?
 		if Hardware::CPU.arm?
 			url "https://github.com/paularlott/scriptling/releases/download/v#{version}/scriptling-darwin-arm64.zip"
-			sha256 "2fd1d8b56d34b90aa279c08e09cc10da6eb817c39f784157a6202103fa6e16e7"
+			sha256 "c28d6ed25cfbd00449b881b32eb5509b523a0c6f2f95235a479fde89f430cd2e"
 		else
 			url "https://github.com/paularlott/scriptling/releases/download/v#{version}/scriptling-darwin-amd64.zip"
-			sha256 "25459119f244b67915dcc90a22bf38838f5fcf105e6c9b680af3a27fcba12f3b"
+			sha256 "f0516bed71718246c257b447ea25fda23f7ee6505e2d9f5c74d334a2412f9e72"
 		end
 	elsif OS.linux?
 		if Hardware::CPU.arm?
 			url "https://github.com/paularlott/scriptling/releases/download/v#{version}/scriptling-linux-arm64.zip"
-			sha256 "25af19dca00c3b3c3c3d758d8039ccfff8da80b98060a521535b1e497953eb73"
+			sha256 "7815ed3ac8d77f1750373df1235b6343fa86b1a20cf6fdaff14db400eb8e71d6"
 		else
 			url "https://github.com/paularlott/scriptling/releases/download/v#{version}/scriptling-linux-amd64.zip"
-			sha256 "c6bf930f891d46c5df422a481054c9959fd72bf0e0b0d3e5e96443d8dd5a4cf4"
+			sha256 "495624aa68b94a1ab0137e7cffeb819c8d6c4b56dd9b6c926abb7bda68a75dbb"
 		end
 	end
 
